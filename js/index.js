3dd19b314c7bc42a0ee1046a4952716591b28e2e
arlan <arlanjose@gmail.com>
2024-09-22 10:27:54 -0300

    feat: tech and general version

--- js/index.js
+++ js/index.js
@@ -1,26 +1,30 @@
 function validateNavigation(url = null) {
-  console.log("validating", url)
-  if (url.hash == '#theology') {
-    console.log("teologia!")
-    document.querySelectorAll('#description > div:not(#theology)').forEach(e => e.style.display = 'none')
-    document.querySelector('#theology').innerHTML += `<div id="back">
-      <a href="#" i18n="description.back"></a>
-    </div>`
-  } else {
-    document.querySelectorAll('#description > div #back').forEach(e => e.remove())
-    document.querySelectorAll('#description > div:not(#theology)').forEach(e => e.style.display = '')
-  }
+  showItem(url.hash.replace('#', '') || 'tech');
+  
   if (window.setLanguage) {
     window.setLanguage()
   }
+}
+
+function showItem(item = 'tech') {
+  let id = `#${item}`;
+  document.querySelectorAll('#description > div #back').forEach(e => e.remove())
+  if (item != 'general') {
+    document.querySelectorAll(`#description > div:not(${id})`).forEach(e => e.style.display = 'none')
+    document.querySelectorAll(`#description > ${id}`).forEach(e => e.style.display = '')
+    document.querySelector(id).innerHTML += `<div id="back">
+      <a href="#general" i18n="description.back"></a>
+    </div>`
+  } else {
+    document.querySelectorAll(`#description > div:not(${id})`).forEach(e => e.style.display = '')
+  }
+  window.scrollTo(0, 0);
 }
 
 navigation.addEventListener("navigate", (event) => {
   const url = new URL(event.destination.url);
 
   validateNavigation(url);
-
-  console.log("Interceptado", event);
 });
 
 validateNavigation(new URL(window.location.href));
